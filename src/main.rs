6e0b8aa12c736f0dc04ce09a70166f0e5b022a9c
arn-the-long-beard <arn.menant@gmail.com>
2020-10-31 18:29:30 +0100

    feature: can now parse rust file and find Routes enum and extract variant and fields

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ use std::{
     fmt::Display,
     fs::File,
     io,
-    io::{BufRead, BufReader, Lines},
+    io::{BufRead, BufReader, Lines, Read},
     path::Path,
 };
 use structopt::{clap::Arg, StructOpt};
+use syn::Item;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt, Debug)]
@@ -20,15 +21,42 @@ struct Cli {
 
 fn main() -> Result<()> {
     let args: Cli = Cli::from_args();
-    match read_lines(&args.path) {
-        Ok(lines) => {
-            for line in lines {
-                println!("{}", line.unwrap());
+    // match read_lines(&args.path) {
+    //     Ok(lines) => {
+    //         for line in lines {
+    //             println!("{}", line.unwrap());
+    //         }
+    //         Ok(())
+    //     }
+    //     Err(e) => Err(Error::from(e)),
+    // }
+
+    let mut file = File::open(&args.path).expect("Unable to open file");
+
+    let mut src = String::new();
+    file.read_to_string(&mut src).expect("Unable to read file");
+
+    let ast = syn::parse_file(&src)?;
+    for a in ast.items.iter() {
+        if let Item::Enum(found_enum) = a {
+            eprintln!("got enum {} ", found_enum.ident);
+            if found_enum.ident == "Routes" {
+                for v in found_enum.variants.iter() {
+                    println!("routes => {}", v.ident);
+                    if v.fields.len() > 0 {
+                        for f in v.fields.iter() {
+                            if let Some(i) = f.ident.as_ref() {
+                                println!("specific_fields {}", i);
+                            }
+                        }
+                    }
+                }
             }
-            Ok(())
         }
-        Err(e) => Err(Error::from(e)),
     }
+    println!("{} items", ast.items.len());
+
+    Ok(())
 }
 
 fn read_lines(filename: &std::path::PathBuf) -> Result<io::Lines<io::BufReader<File>>> {
