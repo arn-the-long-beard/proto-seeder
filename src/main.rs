8d3ec042e7f47c8d62f956d71fee1b9c8b46a384
arn-the-long-beard <arn.menant@gmail.com>
2020-10-31 19:53:31 +0100

    feature: added copy past code from derive macro to extract attributes information and display how many views, files and guard it will create

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use anyhow::{Context, Error, Result};
+use crate::{module::modules_path, view::variant_view_path_tuple};
+
+use crate::view::variant_guard_path_tuple;
+use convert_case::{Case, Casing};
 use std::{
     fmt::Display,
     fs::File,
@@ -6,9 +9,11 @@ use std::{
     io::{BufRead, BufReader, Lines, Read},
     path::Path,
 };
-use structopt::{clap::Arg, StructOpt};
-use syn::{Attribute, Item, Meta, MetaNameValue};
+use structopt::StructOpt;
+use syn::{Attribute, Error, Item, Lit, LitStr, Meta, MetaNameValue, Result};
 
+mod module;
+mod view;
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt, Debug)]
 struct Cli {
@@ -31,39 +36,83 @@ fn main() -> Result<()> {
     //     Err(e) => Err(Error::from(e)),
     // }
 
-    let mut file = File::open(&args.path).expect("Unable to open file");
+    let mut file = File::open(&args.path)
+        .unwrap_or_else(|_| panic!("Unable to open file , {}", &args.path.to_str().unwrap()));
 
     let mut src = String::new();
     file.read_to_string(&mut src).expect("Unable to read file");
+
+    let mut files_to_create: Vec<String> = vec![]; // each of them should be unique
+    let mut local_views_to_create: Vec<String> = vec![]; // each of them should be unique
+    let mut guards_to_create: Vec<String> = vec![]; // each of them should be unique
 
     let ast = syn::parse_file(&src)?;
     for a in ast.items.iter() {
         if let Item::Enum(found_enum) = a {
             eprintln!("got enum {} ", found_enum.ident);
+
             if found_enum.ident == "Routes" {
+                let future_directory = modules_path(found_enum.attrs.iter());
+
                 for v in found_enum.variants.iter() {
                     println!("routes => {}", v.ident);
 
                     for f in v.fields.iter() {
                         if let Some(i) = f.ident.as_ref() {
-                            println!("specific_fields {}", i);
+                            println!(" specific_fields {}", &i);
                         }
+                        // todo add nested tuple, struct and url payload
+                        // extractor so we re inject them into the files we
+                        // create no update
                     }
 
                     for a in v.attrs.iter() {
                         println!("attributes {}", a.path.get_ident().unwrap());
                     }
+                    if let Some(view) = variant_view_path_tuple(v.ident.clone(), v.attrs.iter()) {
+                        println!("The route will load local view");
+                        local_views_to_create.push(format!("pub fn {}", view.1))
+                    // new parser
+                    // here , reuse
+                    // code from macro
+                    // derive ?
+                    } else {
+                        println!("Got a module");
+                        files_to_create.push(v.ident.to_string().to_case(Case::Snake));
+                    }
+
+                    if let Some(guard) = variant_guard_path_tuple(v.ident.clone(), v.attrs.iter()) {
+                        println!("Got a guard");
+                        guards_to_create.push(format!("pub fn {}", guard.1));
+                    }
                 }
             }
         }
     }
+
     println!("{} items", ast.items.len());
 
+    println!("{} files will be created", files_to_create.len());
+    println!(
+        "{} local views will be created",
+        local_views_to_create.len()
+    );
+    println!("{} guards will be created", guards_to_create.len());
     Ok(())
+
+    // todo add counting maybe ?
 }
 
-fn read_lines(filename: &std::path::PathBuf) -> Result<io::Lines<io::BufReader<File>>> {
-    let file = File::open(filename)
-        .with_context(|| format!("could not read file `{}`", &filename.to_str().unwrap()))?;
-    Ok(io::BufReader::new(file).lines())
+pub fn get_string_from_attribute(attribute_name: &str, attr: &Attribute) -> Result<Option<LitStr>> {
+    if !attr.path.is_ident(attribute_name) {
+        return Ok(None); // not our attribute
+    }
+    match attr.parse_meta()? {
+        Meta::NameValue(MetaNameValue {
+            lit: Lit::Str(name),
+            ..
+        }) => Some(Some(name)),
+        _ => None,
+    }
+    .ok_or_else(|| Error::new_spanned(attr, &format!("expected #[{} = \"...\"]", attribute_name)))
 }
---
+++ src/view.rs
@@ -0,0 +1,98 @@
+use proc_macro_error::{abort, Diagnostic, Level};
+
+use crate::get_string_from_attribute;
+
+use syn::{Attribute, Ident};
+
+/// Get the content of #[view = model_prop => view_function]
+pub fn variant_view_path_tuple(
+    _ident: Ident,
+    attrs: std::slice::Iter<'_, Attribute>,
+) -> Option<(String, String)> {
+    let mut attrs = attrs.filter_map(|attr| match get_string_from_attribute("view", attr) {
+        Ok(op) => op,
+        Err(err) => abort!(Diagnostic::new(Level::Error, err.to_string())),
+    });
+    let view_scope = if attrs.clone().count() > 1 {
+        abort!(Diagnostic::new(
+            Level::Error,
+            "Multiple state path defined.".into()
+        ))
+    } else if let Some(name) = attrs.next() {
+        name.value()
+    } else {
+        "".to_string()
+    };
+    if view_scope.is_empty() {
+        None
+    } else {
+        let string_to_parse = view_scope;
+        let view_scope_string: Vec<&str> = string_to_parse.split("=>").collect();
+        let mut view_scope_string_iter = view_scope_string.iter();
+        let view_path = view_scope_string_iter.next().unwrap_or_else(|| {
+            panic!(
+                "expect path for  #[view_path = MODEL_PROP => VIEW] but got this {:?}",
+                string_to_parse
+            )
+        });
+        let view_init = view_scope_string_iter.next().unwrap_or_else(|| {
+            panic!(
+                "expect view for  #[view_path = MODEL_PROP => VIEW] but got this {:?}",
+                string_to_parse
+            )
+        });
+        Some((view_path.trim().to_string(), view_init.trim().to_string()))
+    }
+}
+pub fn variant_guard_path_tuple(
+    _ident: Ident,
+    attrs: std::slice::Iter<'_, Attribute>,
+) -> Option<(String, String, String)> {
+    let mut attrs = attrs.filter_map(|attr| match get_string_from_attribute("guard", attr) {
+        Ok(op) => op,
+        Err(err) => abort!(Diagnostic::new(Level::Error, err.to_string())),
+    });
+    let guard_scope = if attrs.clone().count() > 1 {
+        abort!(Diagnostic::new(
+            Level::Error,
+            "Multiple guard defined are not supported.".into()
+        ))
+    } else if let Some(name) = attrs.next() {
+        name.value()
+    } else {
+        "".to_string()
+    };
+    if guard_scope.is_empty() {
+        None
+    } else {
+        let string_to_parse = guard_scope;
+        let guard_scope_string: Vec<&str> = string_to_parse.split("=>").collect();
+        let mut guard_scope_string_iter = guard_scope_string.iter();
+        let guard_path = guard_scope_string_iter.next().unwrap_or_else(|| {
+            panic!(
+                "expect path for  #[guard_path = PATH => GUARD_FUNCTION => REDIRECT_VIEW] but got \
+                 this {:?}",
+                string_to_parse
+            )
+        });
+        let guard_function = guard_scope_string_iter.next().unwrap_or_else(|| {
+            panic!(
+                "expect path for  #[guard_path = PATH => GUARD_FUNCTION => REDIRECT_VIEW] but got \
+                 this {:?}",
+                string_to_parse
+            )
+        });
+        let guard_redirect = guard_scope_string_iter.next().unwrap_or_else(|| {
+            panic!(
+                "expect path for  #[guard_path = PATH => GUARD_FUNCTION => REDIRECT_VIEW] but got \
+                 this {:?}",
+                string_to_parse
+            )
+        });
+        Some((
+            guard_path.trim().to_string(),
+            guard_function.trim().to_string(),
+            guard_redirect.trim().to_string(),
+        ))
+    }
+}
