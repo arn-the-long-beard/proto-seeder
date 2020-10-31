494701e184db6c6117986644174cb449f5a1ccd4
arn-the-long-beard <arn.menant@gmail.com>
2020-10-31 17:44:17 +0100

    style: ran cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use structopt::StructOpt;
-use structopt::clap::Arg;
-use std::fs::File;
-use std::io::{BufReader, BufRead,  Lines};
-use anyhow::{Context, Result, Error};
-use std::io;
-use std::path::Path;
-use std::fmt::Display;
+use anyhow::{Context, Error, Result};
+use std::{
+    fmt::Display,
+    fs::File,
+    io,
+    io::{BufRead, BufReader, Lines},
+    path::Path,
+};
+use structopt::{clap::Arg, StructOpt};
 
 /// Search for a pattern in a file and display the lines that contain it.
-#[derive(StructOpt, Debug) ]
+#[derive(StructOpt, Debug)]
 struct Cli {
     /// The pattern to look for
     pattern: String,
@@ -18,24 +19,20 @@ struct Cli {
 }
 
 fn main() -> Result<()> {
-    let args  : Cli  = Cli::from_args();
-
-
-    match  read_lines(&args.path) {
-        Ok(lines) =>
-            {
-                for line in lines {
-                    println!("{}",line.unwrap());
-                }
-                Ok(())
+    let args: Cli = Cli::from_args();
+    match read_lines(&args.path) {
+        Ok(lines) => {
+            for line in lines {
+                println!("{}", line.unwrap());
             }
-        Err(e) => { Err(Error::from(e))}
+            Ok(())
+        }
+        Err(e) => Err(Error::from(e)),
     }
 }
-        // Consumes the iterator, returns an (Optional) String
-
 
 fn read_lines(filename: &std::path::PathBuf) -> Result<io::Lines<io::BufReader<File>>> {
-    let file = File::open(filename).with_context(|| format!("could not read file `{}`", &filename.to_str().unwrap()))?;
+    let file = File::open(filename)
+        .with_context(|| format!("could not read file `{}`", &filename.to_str().unwrap()))?;
     Ok(io::BufReader::new(file).lines())
 }
