b638095c040c532c453ad600ff25f9ec1499649f
arn-the-long-beard <arn.menant@gmail.com>
2020-11-20 13:33:28 +0100

    doc: added example app

---
+++ examples/backbone_app/src/models/user.rs
@@ -0,0 +1,46 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize, Default, Clone)]
+pub struct LoggedData {
+    pub first_name: String,
+    pub last_name: String,
+    username: String,
+    email: String,
+    pub role: Role,
+}
+
+impl LoggedData {
+    pub fn new(
+        first_name: &str, last_name: &str, username: &str, email: &str, role: Role,
+    ) -> Self {
+        LoggedData {
+            first_name: first_name.to_string(),
+            last_name: last_name.to_string(),
+            username: username.to_string(),
+            email: email.to_string(),
+            role,
+        }
+    }
+}
+
+impl LoggedData {
+    pub fn username(&self,) -> &str {
+        &self.username
+    }
+
+    pub fn email(&self,) -> &str {
+        &self.email
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub enum Role {
+    StandardUser,
+    Admin,
+}
+
+impl Default for Role {
+    fn default() -> Self {
+        Role::StandardUser
+    }
+}
---
+++ examples/backbone_app/src/request.rs
@@ -0,0 +1,11 @@
+#[derive(Debug)]
+pub enum State<T,> {
+    Success(T,),
+    IsPending(bool,),
+}
+
+impl<T,> Default for State<T,> {
+    fn default() -> Self {
+        State::IsPending(false,)
+    }
+}
