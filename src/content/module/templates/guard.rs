5f7e3fc0498b6f1be782e9e723a66cc731040642
arn-the-long-beard <arn.menant@gmail.com>
2021-02-04 12:58:41 +0100

    fix: added log for guard template in guard.rs

--- src/content/module/templates/guard.rs
+++ src/content/module/templates/guard.rs
@@ -6,7 +6,7 @@ pub const _GUARD_TEMPLATE: &str = r###"
 pub fn GUARD_NAME(PAYLOAD) -> Option<bool>
 {
 if 1+1 ==2 {
- Err("Need to implement nice conditions");
+ log!("Need to implement nice conditions");
 }
 
 None
